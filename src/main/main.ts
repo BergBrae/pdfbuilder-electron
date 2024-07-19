15d1b5bfc6f0390cead54ddae9d7813131fc0a3a
Brady Berg <Bergbrae@msu.edu>
2024-07-19 10:15:55 -0400

    Remove dev extension that is broken

--- src/main/main.ts
+++ src/main/main.ts
@@ -105,9 +105,9 @@ const installExtensions = async () => {
 }
 
 const createWindow = async () => {
-  if (isDebug) {
-    await installExtensions()
-  }
+  // if (isDebug) {
+  //   await installExtensions()
+  // }
 
   const RESOURCES_PATH = app.isPackaged
     ? path.join(process.resourcesPath, 'assets')
