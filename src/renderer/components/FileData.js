2586e2ba2d767534b81bb29003046961fb20409d
Brady Berg <Bergbrae@msu.edu>
2024-08-26 13:36:12 -0400

    chore: Remove ESLint configuration files and VSCode settings

--- src/renderer/components/FileData.js
+++ src/renderer/components/FileData.js
@@ -1,19 +1,22 @@
 // FileData.js
-import React from 'react'
-import { Col, Card } from 'react-bootstrap'
+import React from 'react';
+import { Col, Card } from 'react-bootstrap';
 
-export default function FileData ({ fileData }) {
-  const { file_path, num_pages } = fileData
-  const fileName = file_path.split('\\').pop()
+export default function FileData({ fileData }) {
+  const { file_path, num_pages } = fileData;
+  const fileName = file_path.split('\\').pop();
 
   return (
     <Col xs={12} sm={12} md={4} lg={4}>
-      <Card className='mb-4'>
+      <Card className="mb-4">
         <Card.Body>
-          <p className='filename'>{fileName}</p>
-          <p>{num_pages}{num_pages > 1 ? ' Pages' : ' Page'}</p>
+          <p className="filename">{fileName}</p>
+          <p>
+            {num_pages}
+            {num_pages > 1 ? ' Pages' : ' Page'}
+          </p>
         </Card.Body>
       </Card>
     </Col>
-  )
+  );
 }
