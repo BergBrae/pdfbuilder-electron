cd09f3661bcc4b9940dbee437d22ced2a4aa50ee
Brady Berg <Bergbrae@msu.edu>
2024-09-03 13:39:29 -0400

    Add button to "Bookmark files with filename"

--- src/renderer/components/FileType.js
+++ src/renderer/components/FileType.js
@@ -1,100 +1,154 @@
 // FileType.js
-import React, { useState, useEffect } from 'react'
-import BookmarkIcon from './BookmarkIcon'
-import { FaFilePdf } from 'react-icons/fa'
-import { Form, Container, Button, Row, Accordion } from 'react-bootstrap'
-import FileData from './FileData'
-import { handleAPIUpdate } from './utils'
+import React, { useState, useEffect } from 'react';
+import BookmarkIcon from './BookmarkIcon';
+import { FaFilePdf } from 'react-icons/fa';
+import { Form, Container, Button, Row, Accordion } from 'react-bootstrap';
+import FileData from './FileData';
+import { handleAPIUpdate } from './utils';
+const path = require('path');
 
 const FileIcon = (
-  <span className='pdf-icon align-content-center'>
-    <FaFilePdf className='mb-2 ms-1 mt-1' />
-    <span className='m-2'>PDF File</span>
+  <span className="pdf-icon align-content-center">
+    <FaFilePdf className="mb-2 ms-1 mt-1" />
+    <span className="m-2">PDF File</span>
   </span>
-)
+);
 
-function FileType ({ file, onFileChange, onDelete, parentDirectorySource }) {
-  const [directorySource, setDirectorySource] = useState(file.directory_source)
-  const [filenameText, setFilenameText] = useState(file.filename_text_to_match)
+function FileType({ file, onFileChange, onDelete, parentDirectorySource }) {
+  const [directorySource, setDirectorySource] = useState(file.directory_source);
+  const [filenameText, setFilenameText] = useState(file.filename_text_to_match);
 
   useEffect(() => {
-    updateFile({ ...file, directory_source: directorySource, filename_text_to_match: filenameText })
-  }, [directorySource, filenameText])
+    updateFile({
+      ...file,
+      directory_source: directorySource,
+      filename_text_to_match: filenameText,
+    });
+  }, [directorySource, filenameText]);
 
   const updateFile = (updatedFile) => {
     handleAPIUpdate(
       `http://localhost:8000/filetype?parent_directory_source=${parentDirectorySource}`,
       updatedFile,
       onFileChange,
-      (error) => console.error('Failed to update file type with the API', error)
-    )
-  }
+      (error) =>
+        console.error('Failed to update file type with the API', error),
+    );
+  };
 
   const handleDirectoryChange = (e) => {
-    const newDirectorySource = e.target.value
-    setDirectorySource(newDirectorySource)
-  }
+    const newDirectorySource = e.target.value;
+    setDirectorySource(newDirectorySource);
+  };
 
   const handleFilenameChange = (e) => {
-    const newFilenameText = e.target.value
-    setFilenameText(newFilenameText)
-  }
+    const newFilenameText = e.target.value;
+    setFilenameText(newFilenameText);
+  };
 
   const handleBookmarkChange = (newBookmarkName) => {
-    updateFile({ ...file, bookmark_name: newBookmarkName || null })
-  }
+    updateFile({ ...file, bookmark_name: newBookmarkName || null });
+  };
 
   const handleDelete = () => {
-    onDelete(file.id)
-  }
+    onDelete(file.id);
+  };
 
   const handleFileDataChange = (updatedFileData) => {
-    const updatedFiles = file.files.map((fileData) => (fileData.id === updatedFileData.id ? updatedFileData : fileData))
-    onFileChange({ ...file, files: updatedFiles })
-  }
+    const updatedFiles = file.files.map((fileData) =>
+      fileData.id === updatedFileData.id ? updatedFileData : fileData,
+    );
+    onFileChange({ ...file, files: updatedFiles });
+  };
+
+  const handleBookmarkFilesWithFilename = () => {
+    const updatedFiles = file.files.map((fileData) => ({
+      ...fileData,
+      bookmark_name: path
+        .basename(fileData.file_path.replaceAll('\\', '/'))
+        .replaceAll('.pdf', '')
+        .replaceAll('-', ' ')
+        .replaceAll('.PDF', '')
+        .replaceAll('_', ' '),
+    }));
+    onFileChange({ ...file, files: updatedFiles });
+  };
 
   return (
     <Accordion className={file.files.length ? 'file-found' : 'file-not-found'}>
       <Accordion.Header>
         <Container>
-          <div className='d-flex justify-content-between'>
+          <div className="d-flex justify-content-between">
             <BookmarkIcon
               isBookmarked={!!file.bookmark_name}
               bookmarkName={file.bookmark_name}
               onBookmarkChange={handleBookmarkChange}
             />
-            <Button className='x' variant='danger' size='sm' onClick={handleDelete}>
+            <Button
+              className="x"
+              variant="danger"
+              size="sm"
+              onClick={handleDelete}
+            >
               X
             </Button>
           </div>
           <br />
           {FileIcon}
-          <span className='ms-5'>
-            A PDF in <input className='narrow-input' value={directorySource} onChange={handleDirectoryChange} /> containing{' '}
-            <input className='wide-input' value={filenameText} onChange={handleFilenameChange} /> in the filename.
+          <span className="ms-5">
+            A PDF in{' '}
+            <input
+              className="narrow-input"
+              value={directorySource}
+              onChange={handleDirectoryChange}
+            />{' '}
+            containing{' '}
+            <input
+              className="wide-input"
+              value={filenameText}
+              onChange={handleFilenameChange}
+            />{' '}
+            in the filename.
             <Form.Check
               inline
-              type='switch'
-              id='page-numbers-switch'
-              label='Add Page Numbers'
+              type="switch"
+              id="page-numbers-switch"
+              label="Add Page Numbers"
               checked={file.will_have_page_numbers}
-              onChange={() => updateFile({ ...file, will_have_page_numbers: !file.will_have_page_numbers })}
-              className='ms-5'
+              onChange={() =>
+                updateFile({
+                  ...file,
+                  will_have_page_numbers: !file.will_have_page_numbers,
+                })
+              }
+              className="ms-5"
             />
           </span>
         </Container>
       </Accordion.Header>
       {file.files.length > 0 && (
         <Accordion.Body>
+          <Button
+            variant="secondary"
+            size="sm"
+            className="mb-2"
+            onClick={handleBookmarkFilesWithFilename}
+          >
+            Bookmark Files with Filename
+          </Button>
           <Row>
             {file.files.map((fileData) => (
-              <FileData key={fileData.id} fileData={fileData} onFileDataChange={handleFileDataChange}/>
+              <FileData
+                key={fileData.id}
+                fileData={fileData}
+                onFileDataChange={handleFileDataChange}
+              />
             ))}
           </Row>
         </Accordion.Body>
       )}
     </Accordion>
-  )
+  );
 }
 
-export default FileType
+export default FileType;
