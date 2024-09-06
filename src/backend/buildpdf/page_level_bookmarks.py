541732e616fc9d125a7aa1591b9817b3d26e5f24
Brady Berg <bradyberg@meritlabs.com>
2024-09-06 10:47:41 -0400

    Add SAMPLEID rule and remove consecutive bookmarks

--- src/backend/buildpdf/page_level_bookmarks.py
+++ src/backend/buildpdf/page_level_bookmarks.py
@@ -1,6 +1,17 @@
 from PyPDF2 import PdfReader
 from schema import BookmarkItem
 from utils.qualify_filename import qualify_filename
+import re
+
+
+def remove_consecutive_bookmarks(bookmarks):
+    new_bookmarks = []
+    for i, bookmark in enumerate(bookmarks):
+        if i == 0:
+            new_bookmarks.append(bookmark)
+        elif bookmark.title != bookmarks[i - 1].title:
+            new_bookmarks.append(bookmark)
+    return new_bookmarks
 
 
 def get_page_level_bookmarks(pdf, rules, parent_bookmark, parent_page_num):
@@ -8,7 +19,17 @@ def get_page_level_bookmarks(pdf, rules, parent_bookmark, parent_page_num):
     for page in range(len(pdf.pages)):
         text = pdf.pages[page].extract_text()
         for rule in rules:
-            if qualify_filename(rule["rule"], text):
+            if rule["rule"] == "SAMPLEID" == rule["bookmark_name"]:
+                expression = re.compile(r"S\d{5}\.\d{2}")
+                match = expression.search(text)
+                if match:
+                    bookmark = BookmarkItem(
+                        title=match.group(),
+                        page=parent_page_num + page,
+                        parent=parent_bookmark,
+                    )
+                    bookmarks.append(bookmark)
+            elif qualify_filename(rule["rule"], text):
                 bookmark = BookmarkItem(
                     title=rule["bookmark_name"],
                     page=parent_page_num + page,
@@ -16,17 +37,6 @@ def get_page_level_bookmarks(pdf, rules, parent_bookmark, parent_page_num):
                 )
                 bookmarks.append(bookmark)
 
-    # idx_to_remove = (
-    #     []
-    # )  # Consecutive bookmarks with page numbers that are one apart should be removed
-    # for i in range(len(bookmarks) - 1):
-    #     j = i + 1
-    #     if (bookmarks[i].page == bookmarks[j].page - 1) and (
-    #         bookmarks[i].title == bookmarks[j].title
-    #     ):
-    #         idx_to_remove.append(j)
-
-    # for idx in reversed(idx_to_remove):
-    #     bookmarks.pop(idx)
+    bookmarks = remove_consecutive_bookmarks(bookmarks)
 
     return bookmarks
