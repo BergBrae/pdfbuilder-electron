308dd5f43ad535f8016dbc6bc9d2c944d7209142
Brady Berg <bradyberg@meritlabs.com>
2024-09-23 11:13:38 -0400

    “########.d" bookmark as " S#####.##**

--- src/backend/buildpdf/page_level_bookmarks.py
+++ src/backend/buildpdf/page_level_bookmarks.py
@@ -15,10 +15,21 @@ def remove_consecutive_bookmarks(bookmarks):
     return new_bookmarks
 
 
+def convert_sample_id_forms(text):
+    # In some of the socuments, the sample id is listed as 1234567.d instead of S12345.01
+    # This function converts the sample id to the correct format
+    pattern = r"\d{7}\.d"
+    matches = re.findall(pattern, text)
+    for match in matches:
+        text = text.replace(match, f"S{match[:5]}.{match[5:7]}")
+    return text
+
+
 def get_page_level_bookmarks(pdf, rules, parent_bookmark, parent_page_num):
     bookmarks = []
     for page in range(len(pdf.pages)):
         text = pdf.pages[page].extract_text()
+        text = convert_sample_id_forms(text)
         for rule in rules:
             if (rule["rule"] == "SAMPLEID") and (rule["bookmark_name"] == "SAMPLEID"):
                 expression = re.compile(r"S\d{5}\.\d{2}")
@@ -43,3 +54,7 @@ def get_page_level_bookmarks(pdf, rules, parent_bookmark, parent_page_num):
     bookmarks = remove_consecutive_bookmarks(bookmarks)
 
     return bookmarks
+
+
+if __name__ == "__main__":
+    print(convert_sample_id_forms("Testing testing 1234567.d S12345.67"))
