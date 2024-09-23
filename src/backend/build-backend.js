23c14c2e0b34ca72d9564f6a699af1f937e588a2
Brady Berg <bradyberg@meritlabs.com>
2024-09-23 10:46:39 -0400

    Refactor build-backend.js and todo.md
    - Refactor build-backend.js to check if virtual environment exists before creating it and installing requirements.txt. Also, run PyInstaller using the virtual environment's Python.
    - Update todo.md with new tasks and remove auto update function.

--- src/backend/build-backend.js
+++ src/backend/build-backend.js
@@ -1,12 +1,31 @@
 const { execSync } = require('child_process');
+const fs = require('fs');
 
 try {
-  // Activate the virtual environment and run PyInstaller
-  const activateCommand =
+  // Check if virtual environment exists
+  if (!fs.existsSync('.venv')) {
+    // Create the virtual environment
+    const createVenvCommand =
+      process.platform === 'win32'
+        ? 'python -m venv .venv'
+        : 'python3 -m venv .venv';
+    execSync(createVenvCommand, { stdio: 'inherit', shell: true });
+  }
+
+  // Install requirements.txt
+  const installRequirementsCommand =
     process.platform === 'win32'
-      ? '.venv\\Scripts\\activate && pyinstaller --onefile api.py'
-      : 'source .venv/bin/activate && pyinstaller --onefile api.py';
-  execSync(activateCommand, { stdio: 'inherit', shell: true });
+      ? '.venv\\Scripts\\pip install -r requirements.txt'
+      : '.venv/bin/pip install -r requirements.txt';
+  execSync(installRequirementsCommand, { stdio: 'inherit', shell: true });
+
+  // Run PyInstaller using the virtual environment's Python
+  const runPyInstallerCommand =
+    process.platform === 'win32'
+      ? '.venv\\Scripts\\pyinstaller --onefile api.py'
+      : '.venv/bin/pyinstaller --onefile api.py';
+  execSync(runPyInstallerCommand, { stdio: 'inherit', shell: true });
+
   console.log('Build completed successfully');
 } catch (error) {
   console.error('Error during build:', error);
--- todo.md
+++ todo.md
@@ -9,18 +9,25 @@
 - Debug and fix Patrick's ICF tepmplate. Builds fine when there are no table entries, but fails when there are.
 - When loading template that has SAMPLEID special rule, it does not show as the special rule.
 
-
 ### Commits to do from meeting on 9/19
 
 [Meeting Notes](https://docs.google.com/document/d/1KNUOfTx2cZitwFIFhohV-zp3E4agBitj4SXuCxOgKug/edit?pli=1 "Google Docs")
+
+- Build table of contents from the outline.
+  - i.e. when a section does not exist it should not be in the table of contents.
+- Rearrange metals form 1 by sort(SampleID, file).
+  - Meaning the files will be interwoven, where each change in sample ID is a breakable point.
+- Update Help Section
+
+
+# Todo Today
+
 
 - Update merit ID special bookmark rule.
   - “########.d" bookmark as " S#####.##**.
   - not "Report Id: S#####.##".
 - Allow PDF to build when a section's base directory is not present.
   - e.g. There is no PFAS folder but there is a PFAS section.
-- Build table of contents from the outline.
-  - i.e. when a section does not exist it should not be in the table of contents.
-- Rearrange metals form 1 by sort(SampleID, file).
-  - Meaning the files will be interwoven, where each change in sample ID is a breakable point.
-- Update Help Section
+- When loading template with special rule, fix showing as merit special rule.
+- Send Email to May asking for a time to meet
+- remove auto update function
