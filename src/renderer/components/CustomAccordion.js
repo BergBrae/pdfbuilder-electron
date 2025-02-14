651e21d5788d2e00d7705b11d852b1cdfbb09a9c
Brady Berg <bradyberg@meritlabs.com>
2025-02-14 12:28:34 -0500

    Fix accordion expansion/collapsing control

--- src/renderer/components/CustomAccordion.js
+++ src/renderer/components/CustomAccordion.js
@@ -11,44 +11,39 @@ const CustomAccordion = ({
 }) => {
   const [isExpanded, setIsExpanded] = useState(defaultExpanded);
 
-  const handleToggle = useAccordionButton(eventKey, () => {
+  const handleToggle = () => {
     setIsExpanded(!isExpanded);
-  });
+  };
 
   return (
-    <Accordion
-      defaultActiveKey={defaultExpanded ? eventKey : undefined}
-      className="mb-3"
-    >
-      <Card className={className}>
-        <Card.Header className="py-2">
-          <div className="d-flex justify-content-between align-items-center">
-            <div
-              style={{ flex: 1 }}
-              onClick={handleToggle}
-              role="button"
-              className="d-flex justify-content-between align-items-center"
-            >
-              {header}
-            </div>
-            {!defaultExpanded && (
-              <div style={{ cursor: 'pointer', marginLeft: '1rem' }}>
-                <span onClick={handleToggle}>
-                  {isExpanded ? (
-                    <MdExpandLess size={24} />
-                  ) : (
-                    <MdExpandMore size={24} />
-                  )}
-                </span>
-              </div>
-            )}
+    <Card className={`${className} mb-3`}>
+      <Card.Header className="py-2">
+        <div className="d-flex justify-content-between align-items-center">
+          <div
+            style={{ flex: 1 }}
+            onClick={handleToggle}
+            role="button"
+            className="d-flex justify-content-between align-items-center"
+          >
+            {header}
           </div>
-        </Card.Header>
-        <Accordion.Collapse eventKey={eventKey}>
-          <Card.Body className="py-3">{children}</Card.Body>
-        </Accordion.Collapse>
-      </Card>
-    </Accordion>
+          {!defaultExpanded && (
+            <div style={{ cursor: 'pointer', marginLeft: '1rem' }}>
+              <span onClick={handleToggle}>
+                {isExpanded ? (
+                  <MdExpandLess size={24} />
+                ) : (
+                  <MdExpandMore size={24} />
+                )}
+              </span>
+            </div>
+          )}
+        </div>
+      </Card.Header>
+      <div className={`collapse ${isExpanded ? 'show' : ''}`}>
+        <Card.Body className="py-3">{children}</Card.Body>
+      </div>
+    </Card>
   );
 };
 
